458b62ca1db9b9c6c0bff475754682ad6f8535d8
Louis Varley <Louis.Varley@landscapeinstitute.org>
2021-06-19 17:14:59 +0100

    Force Rename

---
+++ app2/Config.php
@@ -0,0 +1,22 @@
+
+<?php			
+
+/* General */
+
+define('_FIRST_LAUNCH','FALSE');
+define('_SOLD_STATUS','5');
+define('_SALE_STATUS','1');
+define('_BOUGHT_OUT_STATUS','6');
+
+
+
+
+/* Database Config */
+define('_DB_HOST','localhost');
+define('_DB_NAME','retroseller');
+define('_DB_USER','root');
+define('_DB_PASSWORD','');
+define('_DB_PORT','3306');
+define('_DB_DUMPER','C:\wamp64\bin\mysql\mysql8.0.22\bin\mysqldump.exe');		
+
+?>
