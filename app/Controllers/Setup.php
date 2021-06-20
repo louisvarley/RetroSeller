cd37d130d7290d7a1352de6207d89609593aff30
Louis Varley <Louis.Varley@landscapeinstitute.org>
2021-06-20 18:36:12 +0100

    Update Setup.php

--- app/Controllers/Setup.php
+++ app/Controllers/Setup.php
@@ -36,6 +36,8 @@ class Setup extends \Core\Controller
 		
 		if($this->isPOST()){
 
+			sessionManager()->start();
+
 			$conn = new \mysqli($this->post['db_host'], $this->post['db_user'], $this->post['db_password']);
 			if ($conn->connect_error) {
 				toastManager()->throwError("Error...", ("MySQL Connection failed: " . $conn->connect_error));
@@ -106,6 +108,57 @@ define('_DB_DUMPER','mysqldump');
 		
 		entityManager()->flush();
 		
+		
+		$sql = '	
+				
+			CREATE OR REPLACE VIEW rs_purchase_categories_vw AS
+			SELECT   id,
+					 name,
+					 parent_id,
+					 path,
+					 depth,
+					 color
+			FROM     (
+							SELECT id,
+								   name,
+								   parent_id,
+								   name  AS path,
+								   color AS color,
+								   0     AS depth
+							FROM   rs_purchase_categories
+							WHERE  parent_id IS NULL
+							UNION ALL
+							SELECT    t2.id                                     AS id,
+									  t2.name                                   AS name,
+									  t1.id                                     AS parent_id,
+												concat(t1.name, " > ", t2.name) AS path,
+									  t2.color                                  AS color,
+									  1                                         AS depth
+							FROM      rs_purchase_categories t1
+							LEFT JOIN rs_purchase_categories t2
+							ON        t2.parent_id = t1.id
+							WHERE     t1.parent_id IS NULL
+							UNION ALL
+							SELECT    t3.id                                                     AS id,
+									  t3.name                                                   AS name,
+									  t2.id                                                     AS parent_id,
+												concat(t1.name, " > ", t2.name, " > ", t3.name) AS path,
+									  t3.color                                                  AS color,
+									  2                                                         AS depth
+							FROM      rs_purchase_categories t2
+							LEFT JOIN rs_purchase_categories t3
+							ON        t3.parent_id = t2.id
+							LEFT JOIN rs_purchase_categories t1
+							ON        t2.parent_id = t1.id
+							WHERE     t2.parent_id IS NOT NULL
+							AND       t3.id IS NOT NULL ) a
+			ORDER BY a.path,
+					 a.name ASC ';
+						
+		$connection = entityManager()->getConnection();
+		$statement = $connection->prepare($sql);
+		$statement->execute();		 		
+	
 		toastManager()->throwSuccess("Ready to Rock and Roll...", "You are setup and ready to go");
 		header('Location: /login');
 		
