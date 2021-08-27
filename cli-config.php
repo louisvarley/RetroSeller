9908676172ec4091a355eab36a9b305fa07ad985
Louis Varley <30378727+louisvarley@users.noreply.github.com>
2021-08-27 18:00:59 +0100

    Update cli-config.php

--- cli-config.php
+++ cli-config.php
@@ -2,8 +2,7 @@
 
 use Core\DataAccess;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use \Core\Services\EntityService as Entities;
 
 require __DIR__ . '/core/Globals.php';
 
-return ConsoleRunner::createHelperSet(Entities::em());
+return ConsoleRunner::createHelperSet(EntityService::em());
