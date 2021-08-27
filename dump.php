d560648ecca6862bce7321120c2a2673984ca0e3
Louis Varley <30378727+louisvarley@users.noreply.github.com>
2021-08-27 17:59:38 +0100

    Update dump.php

--- dump.php
+++ dump.php
@@ -1,6 +1,6 @@
 <?php 
 
-require __DIR__ . '/Core/Globals.php';
+require __DIR__ . '/core/Globals.php';
 $d = new \DateTime('now');
 if(!file_exists("dumps")) mkdir("dumps");
 exec(_DB_DUMPER . ' --user=' . _DB_USER . ' --password=' . _DB_PASSWORD .' --host=' . _DB_HOST . ' ' . _DB_NAME . ' > dumps\\' . $d->format('Y-m-d.G.i') . '.sql');
